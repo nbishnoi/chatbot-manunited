67fb9dbb4a5fe22548e6563b736958d3aa8462da
nbishnoi <64088648+nbishnoi@users.noreply.github.com>
2020-04-30 15:44:15 +1000

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\chatbotnishant-manunited-Web-Deploy.pubxml -p:Password=lHzcHEGA08GvSaoHx2ovkHlMtnmhNpNtbPdADSomZvRvt5rJwAYQdJcYi1C1
+
